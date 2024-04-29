6dafde2f95ece1da8dbaad33699208b224193b40
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-04-29 17:07:44 +0200

    [Setting Up Game Panels]

---
+++ Assets/Scripts/GameButtonManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameButtonManager : MonoBehaviour
+{
+    // Home Menu Button
+    public void homeButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Options Menu Button
+    public void optionsButton()
+    {
+        
+    }
+
+    // Input Menu Button
+    public void inputButton()
+    {
+
+    }
+}
