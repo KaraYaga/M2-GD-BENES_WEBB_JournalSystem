7c839fe55224d46cf41c56720db0ad2e54964581
Bilou4000 <claire.benes@artfx.fr>
2024-04-30 16:24:05 +0200

    [feat] rebind

---
+++ Assets/Scripts/ReBinding.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ReBinding : MonoBehaviour
+{
+    [SerializeField] private InputActionReference action;
+    private InputAction inputAction;
+    private InputActionRebindingExtensions.RebindingOperation rebindOperation;
+
+    void Start()
+    {
+        inputAction = action;
+    }
+
+    public void StartRebind() 
+    {
+        inputAction.Disable();
+        rebindOperation = inputAction.PerformInteractiveRebinding()
+            .WithControlsExcluding("<Keyboard>/escape")
+            .OnComplete(operation => RebindComplete());
+
+        rebindOperation.Start();
+        inputAction.Enable();
+    }
+
+    public void RebindComplete()
+    {
+        rebindOperation.Dispose();
+    }
+}
